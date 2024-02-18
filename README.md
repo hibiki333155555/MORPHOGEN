80122adb780ab06fbb77edb71b7ac531799e6fbe
MORI <105579842+hibiki333155555@users.noreply.github.com>
2024-02-18 16:16:14 +0900

    ホーム画面の表示方法がわかりません。muiとtailwindを入れました

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # article0
+
+https://tailwindcss.com/docs/installation
+
+
+npm install @mui/material @emotion/react @emotion/styled
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="bg-cover h-screen w-screen flex sm:flex-row space-x-72 justify-center xl:bg-[url('./assets/home.png')] relative" >
+        <div className="pt-64 pl-8 space-y-8 absolute right-32">
+          <h1 className="text-6xl font-bold text-white">Article</h1>
+        </div>
       </div>
     </>
   )
