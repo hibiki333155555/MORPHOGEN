4a77b364d44359b9f76c009553d7a4d973235f58
andy <takuyarossi77@gmail.com>
2024-02-19 22:20:24 +0900

    commitするディレクトリ間違えたので再コミット

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
