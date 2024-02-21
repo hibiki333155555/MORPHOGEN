4af48c2cdab89e7e5cbdaad07c4112e0c8272fe1
andy <takuyarossi77@gmail.com>
2024-02-21 17:25:08 +0900

    auto routing is kinda good

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "morphogen",
+  description: "we are morphogen",
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
