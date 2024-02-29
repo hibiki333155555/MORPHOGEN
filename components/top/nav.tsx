0794d2edacdf9bb28b4a89e0a921ee0bd3452c98
MORI <105579842+hibiki333155555@users.noreply.github.com>
2024-02-29 22:01:56 +0900

    home-text

--- components/top/nav.tsx
+++ components/top/nav.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export const nav = () => {
     return (
         <>
-            <nav className="text-6xl flex justify-between items-center absolute top-[20%] left-[80%]">
+            <nav className="mt-28 animate-text-pop-up-top text-6xl flex justify-between items-center absolute top-[20%] left-[80%]">
                 <ul className=" font-serif text-stone-800/[.7] font-light">
                     <li className=" mb-8 transition duration-700 ease-in-out hover:text-rose-300"><Link href="/news">news</Link></li>
                     <li className=" mb-8 transition duration-700 ease-in-out hover:text-rose-300"><Link href="/article">article</Link></li>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,8 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       animation: {
-        "slide-in-bck-center": "slide-in-bck-center 6s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both"
+        "slide-in-bck-center": "slide-in-bck-center 6s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both",
+        "text-pop-up-top": "text-pop-up-top 2s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both"
       },
       keyframes: {
           "slide-in-bck-center": {
@@ -26,7 +27,19 @@ const config: Config = {
                   transform: "translateZ(0)",
                   opacity: "1"
               }
-          }
+          },
+          "text-pop-up-top": {
+            "0%": {
+                transform: "translateY(0)",
+                "transform-origin": "50% 50%",
+                "text-shadow": "none"
+            },
+            to: {
+                transform: "translateY(-50px)",
+                "transform-origin": "50% 50%",
+                "text-shadow": "0 1px 0 #ccc, 0 2px 0 #ccc, 0 3px 0 #ccc, 0 4px 0 #ccc, 0 5px 0 #ccc, 0 6px 0 #ccc, 0 7px 0 #ccc, 0 8px 0 #ccc, 0 9px 0 #ccc, 0 50px 30px rgba(0, 0, 0, .1)"
+            }
+        }
       }
     },
   },
